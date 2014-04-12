479325fb2b1721d0ce643d8d0c900bee35cc9a59
Ali Khatami <ali.khatami@markit.com>
2014-04-12 02:37:36 -0600

    More cleanup and updating of packages.

--- Controllers/ElementController.cs
+++ Controllers/ElementController.cs
@@ -9,6 +9,42 @@ namespace MOD.Web.Element.Controllers
 	public abstract class ElementController : Controller
 	{
 		/// <summary>
+		/// Simple struct that contains HTML doctype declarations. Source: http://www.w3schools.com/tags/tag_doctype.asp
+		/// </summary>
+		public struct DocTypes
+		{
+			public const string HTML5 = "<!DOCTYPE html>";
+			/// <summary>
+			/// This DTD contains all HTML elements and attributes, but does NOT INCLUDE presentational or deprecated elements (like font). Framesets are not allowed.
+			/// </summary>
+			public const string HTML_4_01_STRICT = "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01//EN\" \"http://www.w3.org/TR/html4/strict.dtd\">";
+			/// <summary>
+			/// This DTD contains all HTML elements and attributes, INCLUDING presentational and deprecated elements (like font). Framesets are not allowed.
+			/// </summary>
+			public const string HTML_4_01_TRANSITIONAL = "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\" \"http://www.w3.org/TR/html4/loose.dtd\">";
+			/// <summary>
+			/// This DTD is equal to HTML 4.01 Transitional, but allows the use of frameset content.
+			/// </summary>
+			public const string HTML_4_01_FRAMESET = "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Frameset//EN\" \"http://www.w3.org/TR/html4/frameset.dtd\">";
+			/// <summary>
+			/// This DTD contains all HTML elements and attributes, but does NOT INCLUDE presentational or deprecated elements (like font). Framesets are not allowed. The markup must also be written as well-formed XML.
+			/// </summary>
+			public const string XHTML_1_0_STRICT = "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">";
+			/// <summary>
+			/// This DTD contains all HTML elements and attributes, INCLUDING presentational and deprecated elements (like font). Framesets are not allowed. The markup must also be written as well-formed XML.
+			/// </summary>
+			public const string XHTML_1_0_TRANSITIONAL = "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">";
+			/// <summary>
+			/// This DTD is equal to XHTML 1.0 Transitional, but allows the use of frameset content.
+			/// </summary>
+			public const string XHTML_1_0_FRAMESET = "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Frameset//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-frameset.dtd\">";
+			/// <summary>
+			/// This DTD is equal to XHTML 1.0 Strict, but allows you to add modules (for example to provide ruby support for East-Asian languages).
+			/// </summary>
+			public const string XHTML_1_1 = "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.1//EN\" \"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd\">";
+		}
+
+		/// <summary>
 		/// Allows you to pass an Element view, generally a layout document but any IRenderable will do. Will return content as
 		/// text/html, encdoded in UTF8.
 		/// </summary>
@@ -20,6 +56,10 @@ namespace MOD.Web.Element.Controllers
 		/// </returns>
 		protected ActionResult View(IRenderable view)
 		{
+			// write the doc type
+			Response.Write(DocTypes.HTML5);
+
+			// write the view
 			return View(view, "text/html", Encoding.UTF8);
 		}
 
--- Modules/ViewModule.cs
+++ Modules/ViewModule.cs
@@ -12,6 +12,28 @@ namespace MOD.Web.Element.Modules
 			get { return _Container = _Container ?? CreateContainer(); }
 			set { _Container = value; }
 		}
+		
+		/// <summary>
+		/// The tag definition to use to create the container by default
+		/// </summary>
+		private string _TagDefinition;
+
+		/// <summary>
+		/// Create the ViewModule instance with a container of div with no classes or other attributes.
+		/// </summary>
+		public ViewModule() : this("div") { }
+
+		/// <summary>
+		/// Creates the ViewModule instance with a container created with whatever tag definition you pass for the container.
+		/// </summary>
+		/// <param name="nodeDefinition">
+		///	The tag definition you want to create the container with.
+		/// </param>
+		public ViewModule(string nodeDefinition)
+		{
+			this._TagDefinition = nodeDefinition;
+		}
+
 		public virtual Node Render() { return Container; }
 
 		/// <summary>
@@ -20,7 +42,7 @@ namespace MOD.Web.Element.Modules
 		/// <returns>Container Element</returns>
 		protected virtual Element CreateContainer()
 		{
-			return Element.Create("div");
+			return Element.Create(this._TagDefinition);
 		}
 	}
 }
