652622f7f9355d5de7e1dac28b8907e6471a2fb2
Ali Khatami <ali.khatami@markit.com>
2014-04-12 00:18:42 -0600

    Added MVC nuget package and ElementController to make getting started much easier. Also removed and organized using statements to clean up the code even further.

---
+++ Controllers/ElementController.cs
@@ -0,0 +1,65 @@
+﻿using System.Text;
+using System.Web.Mvc;
+
+namespace MOD.Web.Element.Controllers
+{
+	/// <summary>
+	/// ElementController provides necessary View() methods that allow you to display your IRenderable page views and/or modules.
+	/// </summary>
+	public abstract class ElementController : Controller
+	{
+		/// <summary>
+		/// Allows you to pass an Element view, generally a layout document but any IRenderable will do. Will return content as
+		/// text/html, encdoded in UTF8.
+		/// </summary>
+		/// <param name="view">
+		/// The IRenderable instance you want to render as a page.
+		/// </param>
+		/// <returns>
+		/// Returns Content of view.Render().ToString(). With content type of text/html, encoded in UTF8.
+		/// </returns>
+		protected ActionResult View(IRenderable view)
+		{
+			return View(view, "text/html", Encoding.UTF8);
+		}
+
+		/// <summary>
+		/// Allows you to pass an Element view, generally a layout document but any IRenderable will do. Will return content as
+		/// whatever content type you pass, encdoded in UTF8.
+		/// </summary>
+		/// <param name="view">
+		/// The IRenderable instance you want to render as a page.
+		/// </param>
+		/// <param name="contentType">
+		/// The content type you want the page to be served as.
+		/// </param>
+		/// <returns>
+		/// Returns Content of view.Render().ToString(). With content type of whatever you pass, encoded in UTF8
+		/// </returns>
+		protected ActionResult View(IRenderable view, string contentType)
+		{
+			return View(view, contentType, Encoding.UTF8);
+		}
+
+		/// <summary>
+		/// Allows you to pass an Element view, generally a layout document but any IRenderable will do. Will return content as
+		/// whatever content type you pass, encoded as whatever you pass.
+		/// </summary>
+		/// <param name="view">
+		/// The IRenderable instance you want to render as a page.
+		/// </param>
+		/// <param name="contentType">
+		/// The content type you want the page to be served as.
+		/// </param>
+		/// /// <param name="encoding">
+		/// The encoding you want to use for the view.
+		/// </param>
+		/// <returns>
+		/// Returns Content of view.Render().ToString(). With content type of whatever you pass, encoded as whatever you pass.
+		/// </returns>
+		protected ActionResult View(IRenderable view, string contentType, Encoding encoding)
+		{
+			return Content(view.Render().ToString(), contentType, encoding);
+		}
+	}
+}
--- Modules/ViewModule.cs
+++ Modules/ViewModule.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
+﻿
 namespace MOD.Web.Element.Modules
 {
 	public class ViewModule : IRenderable
